1621e3d056359bcfeaf279aaa30f799e301dd1c3
MattiasS <mattias.schoenke@gebit-ms.de>
2021-05-17 17:18:49 +0200

    IO Worker

--- IOLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IOLib
-{
-    public class Class1
-    {
-    }
-}
---
+++ IOLib/IOWorker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace IOLib
+{
+    /// <summary>Class doing some IO work</summary>
+    public class IOWorker
+    {
+        /// <summary>
+        /// Does the work
+        /// </summary>
+        /// <exception cref="IOException">IO might fail rarely</exception>
+        public void DoWork()
+        {
+            var random = new Random();
+
+            // Simulate it sometimes failing
+            if (random.Next(10_000) <= 5) throw new IOException();
+
+            // Simulate some work
+            Thread.Sleep(random.Next(5000, 10000));
+        }
+    }
+}
